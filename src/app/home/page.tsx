dd32f3bb7788e891211612eda50a9c91aaba6724
Jack Fletcher <33032000+jack-fletcher@users.noreply.github.com>
2024-11-27 00:31:36 +0000

    Highlight logged in user on leaderboard

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,6 @@ import { Tasks } from "@/components/gamification/tasks/tasks";
 import Navigation from "@/components/navigation/navigation";
 import { UserCard } from "@/components/user-card/user-card";
 import { redirect } from "next/navigation";
-import { get_all_badge_data } from "../api/database/database";
 
 export default async function Home() {
   const session = await auth()
--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -2,12 +2,14 @@ import { auth } from "@/auth";
 import Navigation from "@/components/navigation/navigation";
 import { randomInt } from "crypto";
 import { redirect } from "next/navigation";
-import { get_user_leaderboard_data } from "../api/database/database";
+import { get_user_id_from_session_email, get_user_leaderboard_data } from "../api/database/database";
 import { GetLevelFromExperience } from "@/components/general_use/utils";
 
 export default async function Home() {
   const session = await auth()
   if (!session) redirect('/login');
+  let logged_in_user_id = null;
+  if(session.user?.email) logged_in_user_id = await get_user_id_from_session_email(session.user.email);
   const userData = await get_user_leaderboard_data();
   userData.forEach((element: { level: number; experience: number; }) => {
     let values = GetLevelFromExperience(element.experience);
@@ -20,16 +22,20 @@ export default async function Home() {
       <Navigation />
       <div className="flex flex-row justify-center">
       <table className="m-5 w-2/3 border">
-        <th className="border m-2">Name</th>
-        <th className="border m-2">Level</th>
-        <th className="border m-2">Total XP</th>
+        <tbody>
+          <tr>
+              <th className="border m-2">Name</th>
+              <th className="border m-2">Level</th>
+              <th className="border m-2">Total XP</th>
+          </tr>
         {userData.map((user) => (
-          <tr className="even:bg-slate-950 odd:bg-slate-600" key={user._id}>
+          <tr className={user._id.toString() === logged_in_user_id.toString() ? "bg-slate-950" : "bg-slate-800"} key={user._id}>
             <td className="border m-2">{user.name}</td>
             <td className="border m-2">{user.level}</td>
             <td className="border m-2">{user.experience}</td>
           </tr>
         ))}
+        </tbody>
       </table>
       </div>
     </main>
